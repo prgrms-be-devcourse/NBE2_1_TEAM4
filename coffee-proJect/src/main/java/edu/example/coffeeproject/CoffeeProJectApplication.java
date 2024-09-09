5ae443b419a3d65575f509b56e9269c1b6533416
your-username <your-email@example.com>
2024-09-10 01:03:51 +0900

    start

---
+++ coffee-proJect/src/main/java/edu/example/coffeeproject/CoffeeProJectApplication.java
@@ -0,0 +1,13 @@
+package edu.example.coffeeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeProJectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeProJectApplication.class, args);
+    }
+
+}
